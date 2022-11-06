b5c98183ce2fbbaf54183248633a0f1c3085cb27
Журавлёв Денис <deniszhict@gmail.com>
2022-11-06 19:36:09 +0300

    remote browser change

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -11,18 +11,19 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
+    final static String env = System.getProperty("environment");
+
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        String env = System.getProperty("environment");
         if (env != null && env.equals("remote")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
         }
 
-        Configuration.browserCapabilities = capabilities;
         new WebDriverProvider().setWebDriverConfiguration();
     }
 
@@ -31,7 +32,6 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        String env = System.getProperty("environment");
         if (env != null && env.equals("remote")) {
             Attach.addVideo();
         }
